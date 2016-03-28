c02a2b976436531c33bcf0d28ee4112ec914ca0d
Santiago Martin <santiagomartinagra@gmail.com>
2016-03-28 19:15:12 +0200

    Ya se muestran los datos del viaje

--- sdi2-12/src/com/sdi/presentation/BeanViajes.java
+++ sdi2-12/src/com/sdi/presentation/BeanViajes.java
@@ -1,12 +1,19 @@
 package com.sdi.presentation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import com.sdi.infrastructure.Factories;
+import com.sdi.model.Application;
+import com.sdi.model.Seat;
+import com.sdi.model.SeatStatus;
 import com.sdi.model.Trip;
+import com.sdi.model.User;
+import com.sdi.persistence.SeatDao;
+import com.sdi.persistence.UserDao;
 
 @ManagedBean(name="viajes")
 @SessionScoped
@@ -14,8 +21,13 @@ public class BeanViajes {
 	
 	List<Trip> viajes;
 	Trip viaje;
+	User promotor;
+	List<User> viajeros;
 
 	public List<Trip> getViajes() {
+		promotor = null;
+		viajeros = new ArrayList<>();
+		
 		if(viajes == null){
 			viajes = Factories.persistence.newTripDao().findAllOpenAndPaxAvailables();
 			System.out.println("Viajes cargados: " + viajes.size());
@@ -37,7 +49,42 @@ public class BeanViajes {
 	}
 	
 	public void loadMoreInfo(){
+		UserDao ud = Factories.persistence.newUserDao();
+		SeatDao sd = Factories.persistence.newSeatDao();
+		
 		System.out.println("Loading cosas...");
+		
+		promotor = ud.findById(viaje.getPromoterId());
+		System.out.println("Promotor: " + promotor.getName());
+		viajeros = new ArrayList<>();
+		
+		List<Application> peticiones = Factories.persistence.newApplicationDao().findByTripId(viaje.getId());
+		
+		for(Application peticion: peticiones){
+			Seat plaza = sd.findByUserAndTrip(peticion.getUserId(), viaje.getId());
+			if(plaza != null && plaza.getStatus().equals(SeatStatus.ACCEPTED)){
+				viajeros.add(ud.findById(plaza.getUserId()));
+			}
+		}
+		
+		System.out.println("Loaded " + viajeros.size() + " viajeros");
+		
+	}
+
+	public User getPromotor() {
+		return promotor;
+	}
+
+	public void setPromotor(User promotor) {
+		this.promotor = promotor;
+	}
+
+	public List<User> getViajeros() {
+		return viajeros;
+	}
+
+	public void setViajeros(List<User> viajeros) {
+		this.viajeros = viajeros;
 	}
 
 }
