05bdc92243e1935429d77b4d1bfb0df2b5c96042
Santiago Martin <santiagomartinagra@gmail.com>
2016-03-31 17:38:04 +0200

    test i18N2 [resolve #31]

--- sdi2-test-12/src/com/sdi/tests/Tests/SDI2_Tests.java
+++ sdi2-test-12/src/com/sdi/tests/Tests/SDI2_Tests.java
@@ -22,7 +22,7 @@ public class SDI2_Tests {
 
 	WebDriver driver; 
 	List<WebElement> elementos = null;
-	
+
 	public SDI2_Tests()
 	{
 	}
@@ -43,234 +43,254 @@ public class SDI2_Tests {
 	}
 
 	//PRUEBAS
-	
+
 	//	1.	[RegVal] Registro de Usuario con datos válidos.
 	@Test	
-    public void t01_RegVal() {
+	public void t01_RegVal() {
 		driver.get("http://localhost:8280/sdi2-12/registrarse.xhtml");
 		new PORegistro().rellenaFormulario(driver, "test",  "test",  "test",  "test@test.com",  "test", "test");
-		
+
 		SeleniumUtils.EsperaCargaPagina(driver, "id", "login", 10); 
-		
-    }
+
+	}
 	//	2.	[RegInval] Registro de Usuario con datos inválidos (contraseñas diferentes).
-    @Test
-    public void t02_RegInval() {
-    	
-    	driver.get("http://localhost:8280/sdi2-12/registrarse.xhtml");
+	@Test
+	public void t02_RegInval() {
+
+		driver.get("http://localhost:8280/sdi2-12/registrarse.xhtml");
 		new PORegistro().rellenaFormulario(driver, "test2",  "test2",  "test2",  "test2@test.com",  "test", "test2");
-		
+
 		SeleniumUtils.EsperaCargaPagina(driver, "class", "ui-messages-error", 10);
 		SeleniumUtils.textoPresentePagina(driver, "Error");
-    
-    }
+
+	}
 	//	3.	[IdVal] Identificación de Usuario registrado con datos válidos.
-    @Test
-    public void t03_IdVal() {
-    	
-    	driver.get("http://localhost:8280/sdi2-12/iniciarSesion.xhtml");
+	@Test
+	public void t03_IdVal() {
+
+		driver.get("http://localhost:8280/sdi2-12/iniciarSesion.xhtml");
 		new POInicioSesion().rellenaFormulario(driver, "test", "test");
-		
+
 		SeleniumUtils.EsperaCargaPagina(driver, "text", "test", 10); 
 		SeleniumUtils.textoPresentePagina(driver, "test@test.com"); 
-    
-    }
+
+	}
 	//	4.	[IdInval] Identificación de usuario registrado con datos inválidos.
-    @Test
-    public void t04_IdInval() {
-    	
-    	driver.get("http://localhost:8280/sdi2-12/iniciarSesion.xhtml");
+	@Test
+	public void t04_IdInval() {
+
+		driver.get("http://localhost:8280/sdi2-12/iniciarSesion.xhtml");
 		new POInicioSesion().rellenaFormulario(driver, "test2", "test");
-		
+
 		SeleniumUtils.EsperaCargaPagina(driver, "class", "ui-messages-error", 10); 
 		SeleniumUtils.textoPresentePagina(driver, "Error");
-    
-    }
+
+	}
 	//	5.	[AccInval] Intento de acceso con URL desde un usuario no público (no identificado). Intento de acceso a vistas de acceso privado. 
-    @Test
-    public void t05_AccInval() {
-    	driver.get("http://localhost:8280/sdi2-12/a/misDatos.xhtml");
-    	SeleniumUtils.EsperaCargaPagina(driver, "text", "List", 10); 
-    	
-    }
+	@Test
+	public void t05_AccInval() {
+		driver.get("http://localhost:8280/sdi2-12/a/misDatos.xhtml");
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "List", 10); 
+
+	}
 	//	6.	[RegViajeVal] Registro de un viaje nuevo con datos válidos.
-    @Test
-    public void t06_RegViajeVal() throws InterruptedException {
-    	
-    	t03_IdVal();
-		
+	@Test
+	public void t06_RegViajeVal() throws InterruptedException {
+
+		t03_IdVal();
+
 		// Sesion iniciada
-		
+
 		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "form-nav-bar:registrarViaje", 2); 
 		elementos.get(0).click();
 		SeleniumUtils.EsperaCargaPagina(driver, "text", "cod", 10);
-		
+
 		elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "form-content:botonRecargaSalida", 2);
 		elementos.get(0).click();
-		
+
 		Thread.sleep(1000);
-		
+
 		elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "form-content:boton", 2);
 		elementos.get(2).click();
 		SeleniumUtils.EsperaCargaPagina(driver, "text", "Info", 10); 
-    
-    }
+
+	}
 	//	7.	[RegViajeInVal] Registro de un viaje nuevo con datos inválidos. 
-    @Test
-    public void t07_RegViajeInVal() throws InterruptedException {
-    	
-    	t03_IdVal();
-		
+	@Test
+	public void t07_RegViajeInVal() throws InterruptedException {
+
+		t03_IdVal();
+
 		// Sesion iniciada
-    	
-    	List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "form-nav-bar:registrarViaje", 2); 
+
+		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "form-nav-bar:registrarViaje", 2); 
 		elementos.get(0).click();
 		SeleniumUtils.EsperaCargaPagina(driver, "text", "cod", 10);
-		
+
 		elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "form-content:botonRecargaSalida", 2);
 		elementos.get(0).click();
-		
+
 		Thread.sleep(1000);
-		
+
 		WebElement element = driver.findElement(By.id("form-content:availablePax"));
 		element.click();
 		element.clear();
 		element.sendKeys("6");
-		
+
 		elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "form-content:boton", 2);
 		elementos.get(2).click();
 		SeleniumUtils.EsperaCargaPagina(driver, "text", "Error", 10); 
-    
-    }
+
+	}
 	//	8.	[EditViajeVal] Edición de viaje existente con datos válidos.
-    @Test
-    public void t08_EditViajeVal() {
-    
-    }
+	@Test
+	public void t08_EditViajeVal() {
+
+	}
 	//	9.	[EditViajeInVal] Edición de viaje existente con datos inválidos.
-    @Test
-    public void t09_EditViajeInVal() {
-    
-    }
+	@Test
+	public void t09_EditViajeInVal() {
+
+	}
 	//	10.	[CancelViajeVal] Cancelación de un viaje existente por un promotor.
-    @Test
-    public void t10_CancelViajeVal() {
-    	
-    	t03_IdVal();
-    	
-    	WebElement element = driver.findElement(By.id("form-nav-bar:misViajes"));
-    	element.click();
-    	
-    	List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "cancelarViaje", 2);
-    	elementos.get(elementos.size() - 1).click();
-    	
-    	SeleniumUtils.EsperaCargaPagina(driver, "class", "ui-messages-info", 10);
-    	SeleniumUtils.textoPresentePagina(driver, "Info");
-    
-    }
+	@Test
+	public void t10_CancelViajeVal() {
+
+		t03_IdVal();
+
+		WebElement element = driver.findElement(By.id("form-nav-bar:misViajes"));
+		element.click();
+
+		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "cancelarViaje", 2);
+		elementos.get(elementos.size() - 1).click();
+
+		SeleniumUtils.EsperaCargaPagina(driver, "class", "ui-messages-info", 10);
+		SeleniumUtils.textoPresentePagina(driver, "Info");
+
+	}
 	//	11.	[CancelMulViajeVal] Cancelación de múltiples viajes existentes por un promotor.
-    @Test
-    public void t11_CancelMulViajeVal() throws InterruptedException {
-        	
-        	t06_RegViajeVal();
-        	WebElement element = driver.findElement(By.id("form-nav-bar:cerrarSesion"));
-        	element.click();
-        	Thread.sleep(1500);
-        	t06_RegViajeVal();
-        	    	
-        	element = driver.findElement(By.id("form-nav-bar:misViajes"));
-        	element.click();
-        	
-        	List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "class", "ui-chkbox-box", 2);
-        	Actions builder = new Actions(driver);
-            builder.moveToElement(elementos.get(elementos.size() - 1)).perform(); 
-            elementos.get(elementos.size() - 1).click();
-            builder.moveToElement(elementos.get(elementos.size() - 2)).perform(); 
-            elementos.get(elementos.size() - 2).click();
-        	
-        	element = driver.findElement(By.id("form-content:botonCancelar"));
-        	element.click();
-        	
-        	SeleniumUtils.EsperaCargaPagina(driver, "class", "ui-messages-info", 10);
-        	SeleniumUtils.textoPresentePagina(driver, "Info");
-    	
-    }
+	@Test
+	public void t11_CancelMulViajeVal() throws InterruptedException {
+
+		t06_RegViajeVal();
+		WebElement element = driver.findElement(By.id("form-nav-bar:cerrarSesion"));
+		element.click();
+		Thread.sleep(1500);
+		t06_RegViajeVal();
+
+		element = driver.findElement(By.id("form-nav-bar:misViajes"));
+		element.click();
+
+		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "class", "ui-chkbox-box", 2);
+		Actions builder = new Actions(driver);
+		builder.moveToElement(elementos.get(elementos.size() - 1)).perform(); 
+		elementos.get(elementos.size() - 1).click();
+		builder.moveToElement(elementos.get(elementos.size() - 2)).perform(); 
+		elementos.get(elementos.size() - 2).click();
+
+		element = driver.findElement(By.id("form-content:botonCancelar"));
+		element.click();
+
+		SeleniumUtils.EsperaCargaPagina(driver, "class", "ui-messages-info", 10);
+		SeleniumUtils.textoPresentePagina(driver, "Info");
+
+	}
 	//	12.	[Ins1ViajeAceptVal] Inscribir en un viaje un solo usuario y ser admitido por el promotor.
-    @Test
-    public void t12_Ins1ViajeAceptVal() {
-    
-    }
+	@Test
+	public void t12_Ins1ViajeAceptVal() {
+
+	}
 	//	13.	[Ins2ViajeAceptVal] Inscribir en un viaje dos usuarios y ser admitidos los dos por el promotor.
-    @Test
-    public void t13_Ins2ViajeAceptVal() {
-    
-    }
+	@Test
+	public void t13_Ins2ViajeAceptVal() {
+
+	}
 	//	14.	[Ins3ViajeAceptInval] Inscribir en un viaje (2 plazas máximo) dos usuarios y ser admitidos los dos y que un tercero intente inscribirse en ese mismo viaje pero ya no pueda por falta de plazas.
-    @Test
-    public void t14_Ins3ViajeAceptInval() {
-    
-    }
+	@Test
+	public void t14_Ins3ViajeAceptInval() {
+
+	}
 	//	15.	[CancelNoPromotorVal] Un usuario no promotor Cancela plaza.
-    @Test
-    public void t15_CancelNoPromotorVal() {
-    
-    }
+	@Test
+	public void t15_CancelNoPromotorVal() {
+
+	}
 	//	16.	[Rech1ViajeVal] Inscribir en un viaje un usuario que será admitido y después rechazarlo por el promotor.
-    @Test
-    public void t16_Rech1ViajeVal() {
-    
-    }
+	@Test
+	public void t16_Rech1ViajeVal() {
+
+	}
 	//	17.	[i18N1] Cambio del idioma por defecto a un segundo idioma. (Probar algunas vistas)
-    @Test
-    public void t17_i18N1() {
-    	
-    	driver.get("http://localhost:8280/sdi2-12/");
-    	
-    	SeleniumUtils.EsperaCargaPagina(driver, "text", "List trips", 10);
-    	
-    	WebElement element = driver.findElement(By.id("form-footer:languajeES"));
-    	element.click();
-    	
-    	SeleniumUtils.EsperaCargaPagina(driver, "text", "Listado de viajes", 10);
-    	
-    	element = driver.findElement(By.id("form-nav-bar:iniciarSesion"));
-    	element.click();
-    	
-    	SeleniumUtils.EsperaCargaPagina(driver, "text", "Contraseña", 10);
-    	
-    	element = driver.findElement(By.id("form-nav-bar:registrarse"));
-    	element.click();
-    	
-    	SeleniumUtils.EsperaCargaPagina(driver, "text", "Nombre", 10);
-    	SeleniumUtils.EsperaCargaPagina(driver, "text", "Apellidos", 10);
-    	SeleniumUtils.EsperaCargaPagina(driver, "text", "Correo electronico", 10);
-    	SeleniumUtils.EsperaCargaPagina(driver, "text", "Contraseña", 10);
-    
-    }
+	@Test
+	public void t17_i18N1() {
+
+		driver.get("http://localhost:8280/sdi2-12/");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "List trips", 10);
+
+		WebElement element = driver.findElement(By.id("form-footer:languajeES"));
+		element.click();
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Listado de viajes", 10);
+
+		element = driver.findElement(By.id("form-nav-bar:iniciarSesion"));
+		element.click();
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Contraseña", 10);
+
+		element = driver.findElement(By.id("form-nav-bar:registrarse"));
+		element.click();
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Nombre", 10);
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Apellidos", 10);
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Correo electronico", 10);
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Contraseña", 10);
+
+	}
 	//	18.	[i18N2] Cambio del idioma por defecto a un segundo idioma y vuelta al idioma por defecto. (Probar algunas vistas)
-    @Test
-    public void t18_i18N2() {
-    
-    }
+	@Test
+	public void t18_i18N2() {
+
+		t17_i18N1();
+
+		WebElement element = driver.findElement(By.id("form-footer:languajeEN"));
+		element.click();
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Name", 10);
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Surname", 10);
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Email", 10);
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Password", 10);
+
+		element = driver.findElement(By.id("form-nav-bar:iniciarSesion"));
+		element.click();
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Password", 10);
+
+		element = driver.findElement(By.id("form-nav-bar:listado"));
+		element.click();
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "List trips", 10);
+
+	}
 	//	19.	[OpFiltrado] Prueba para el filtrado opcional.
-    @Test
-    public void t19_OpFiltrado() {
-    
-    }
+	@Test
+	public void t19_OpFiltrado() {
+
+	}
 	//	20.	[OpOrden] Prueba para la ordenación opcional.
-    @Test
-    public void t20_OpOrden() {
-    
-    }
+	@Test
+	public void t20_OpOrden() {
+
+	}
 	//	21.	[OpPag] Prueba para la paginación opcional.
-    @Test
-    public void t21_OpPag() {
-    
-    }
+	@Test
+	public void t21_OpPag() {
+
+	}
 	//	22.	[OpMante] Prueba del mantenimiento programado opcional.
 
-    @Test
-    public void t22_OpMante() {
-    
-    }
+	@Test
+	public void t22_OpMante() {
+
+	}
 }
