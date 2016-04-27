3feb49ea4fc959f45cd84a7e59576bf0538284d6
Santiago Martin <santiagomartinagra@gmail.com>
2016-04-27 18:13:48 +0200

    Cambios en ServiceRest

--- sdi3-12-WEB/src/com/sdi/rest/ServiceRest.java
+++ sdi3-12-WEB/src/com/sdi/rest/ServiceRest.java
@@ -3,6 +3,7 @@ package com.sdi.rest;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,16 +17,16 @@ public interface ServiceRest {
 
 	@GET
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	@Path("{idUser}")
+	@Path("viajes/{idUser}")
 	public List<Trip> listarViajesUsuario(@PathParam("idUser") long idUser);
 	
 	
 	@GET
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	@Path("{idViaje}")
+	@Path("pendientes/{idViaje}")
 	public List<User> listaUsuariosPendientes(@PathParam("idViaje") long idViaje);
 	
-	@GET
+	@PUT
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	@Path("{idViaje,idUsuario}")
 	public void aceptarViajero(@PathParam("idViaje") long idViaje, @PathParam("idUsuario") long idUsuario);
