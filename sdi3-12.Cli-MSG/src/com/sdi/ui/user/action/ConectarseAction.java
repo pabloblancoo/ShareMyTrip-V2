52ff0ca1a11ee265a37e6f7df7794df651fd7ccd
pabloblancoo <pabloblpa@gmail.com>
2016-04-28 15:03:15 +0200

    Comprueba que exista el viaje al que quiere acceder para ver o enviar mensajes

--- sdi3-12.Cli-MSG/src/com/sdi/ui/user/action/ConectarseAction.java
+++ sdi3-12.Cli-MSG/src/com/sdi/ui/user/action/ConectarseAction.java
@@ -2,6 +2,7 @@ package com.sdi.ui.user.action;
 
 import java.util.List;
 
+import javax.crypto.spec.IvParameterSpec;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
 
@@ -12,29 +13,38 @@ import com.sdi.business.EnviarMensaje;
 import com.sdi.modelo.Trip;
 import com.sdi.util.Contexto;
 
-public class ConectarseAction implements Action{
+public class ConectarseAction implements Action {
 
 	@Override
 	public void execute() throws Exception {
-		GenericType<List<Trip>> listm = new GenericType<List<Trip>>() {};
+		GenericType<List<Trip>> listm = new GenericType<List<Trip>>() {
+		};
 
 		List<Trip> res = ClientBuilder.newClient()
-				.target( Contexto.REST_SERVICE_URL +"viajes-participa/" )
-				.path( Contexto.usuario.getId().toString() )
-				.request()
-				.get()
-				.readEntity( listm );
+				.target(Contexto.REST_SERVICE_URL + "viajes-participa/")
+				.path(Contexto.usuario.getId().toString()).request().get()
+				.readEntity(listm);
 
-		for(Trip viaje : res){
+		for (Trip viaje : res) {
 			Console.println(viaje);
 		}
-		
+
 		long idViaje = Console.readLong("Seleccione id del viaje");
-		String mensaje = Console.readString("Mensaje:");
-		new EnviarMensaje().enviarMensaje(idViaje, Contexto.usuario.getId(),mensaje);
+		boolean existeViaje = false;
+		for (Trip viaje : res) {
+			if (idViaje == viaje.getId()) {
+				existeViaje = true;
+			}
+		}
+		if (existeViaje) {
+			String mensaje = Console.readString("Mensaje:");
+			new EnviarMensaje().enviarMensaje(idViaje,
+					Contexto.usuario.getId(), mensaje);
+		}
 
+		else{
+			Console.println("Viaje no existe");
+		}
 	}
-
-
 
 }
