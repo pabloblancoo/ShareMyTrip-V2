b5e0c49e528ffc58ed4e663352587d6ea22c2299
Santiago Martin <santiagomartinagra@gmail.com>
2016-04-28 09:39:42 +0200

    Añadidas constates con las direcciones

--- sdi3-12.Cli-MSG/src/com/sdi/ui/admin/MainMenu.java
+++ sdi3-12.Cli-MSG/src/com/sdi/ui/admin/MainMenu.java
@@ -7,9 +7,9 @@ import com.sdi.util.LogConfig;
 
 public class MainMenu extends BaseMenu {
 	
-	public static final String REST_SERVICE_URL =
-			"http://localhost:8280/sdi3-12-WEB/rest/ServiceRst/";
-	
+	public static final String JMS_CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
+	public static final String SEND_QUEUE = "jms/queue/SendMgs";
+	public static final String RECIEVE_TOPIC = "jms/topic/ReceiveMgs";
 
 	public MainMenu() {
 		menuOptions = new Object[][] {
