8a802d279d89a3cb1a6eb65638758ecf2615dde0
pabloblancoo <pabloblpa@gmail.com>
2016-05-02 17:50:56 +0200

    Ahora si no se registra correctamente en el Cliente de mensajeria vuelve a pedir las credenciales

--- sdi3-12.Cli-MSG/src/com/sdi/ui/user/MainMenu.java
+++ sdi3-12.Cli-MSG/src/com/sdi/ui/user/MainMenu.java
@@ -10,31 +10,32 @@ import com.sdi.util.Contexto;
 import com.sdi.util.LogConfig;
 
 public class MainMenu extends BaseMenu {
-	
-	
+
 	public MainMenu() {
 		menuOptions = new Object[][] {
-				{ "Usuario - " + Contexto.usuario.getLogin(),	null },
-				{ "Conectarse al grupo de un viaje", 			ConectarseAction.class },
-				{ "Hablar por el grupo", 						GrupoAction.class },
-		};
+				{ "Usuario - " + Contexto.usuario.getLogin(), null },
+				{ "Conectarse al grupo de un viaje", ConectarseAction.class },
+				{ "Hablar por el grupo", GrupoAction.class }, };
 	}
 
 	public static void main(String[] args) {
-		
+
 		LogConfig.config();
-		
-		try {
-			new IniciarSesionAction().execute();
-		} catch (Exception e) {
-			Console.println("Usuario y/o password incorrecto.");
+		boolean signIn = false;
+		while (!signIn) {
+			try {
+				new IniciarSesionAction().execute();
+				signIn = true;
+			} catch (Exception e) {
+				Console.println("Usuario y/o password incorrecto.");
+				signIn = false;
+			}
 		}
-		
-		if(Contexto.usuario != null){
+		if (Contexto.usuario != null) {
 			new MainMenu().execute();
 
 		}
-				
+
 	}
 
 }
